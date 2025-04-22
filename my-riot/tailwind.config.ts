cd9db8fc39eca070e0691a08322526e11587f539
momo <rhwoah7190@gmail.com>
2025-04-22 16:40:46 +0900

    # 반응형 디자인 커스텀화

--- my-riot/tailwind.config.ts
+++ my-riot/tailwind.config.ts
@@ -12,6 +12,13 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1080px", // lg 브레이크포인트를 1080px로 변경
+        xl: "1280px",
+        "2xl": "1536px",
+      },
     },
   },
   plugins: [require("daisyui")],
